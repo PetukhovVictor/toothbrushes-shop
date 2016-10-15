46335288eb14da9db883445545e03de2a759f5f2
Victor <i@victor.am>
2016-10-15 14:18:43 +0300

    - refactor back-end folders and files structure - made front-end folders and files structure (js & css files) - added templates - remove jQuery & bootstrap

---
+++ app/engine/application/bootstrap.php
@@ -0,0 +1,3 @@
+<?
+
+require_once APP_DIR . "/config.php";
---
+++ app/public/index.php
@@ -0,0 +1,10 @@
+<?
+
+define("HOME_DIR", $_SERVER["DOCUMENT_ROOT"]);
+define("SYS_DIR", HOME_DIR . "/engine/system");
+define("APP_DIR", HOME_DIR . "/engine/application");
+define("FRONT_CONTROLLERS_DIR", APP_DIR . "/front_controllers");
+define("BACK_CONTROLLERS_DIR", APP_DIR . "/back_controllers");
+define("TEMPLATES_DIR", APP_DIR . "/templates");
+
+require_once APP_DIR . "/bootstrap.php";
